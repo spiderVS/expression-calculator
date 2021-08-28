8294f630a71cbee8fd001da0ad71126777d53800
spiderVS <fuzzy003@gmail.com>
2021-08-28 16:58:46 +0300

    feat: add solve with some errors

--- src/index.js
+++ src/index.js
@@ -4,7 +4,65 @@ function eval() {
 }
 
 function expressionCalculator(expr) {
-    // write your solution here
+
+  operPriortity = {
+    '+': 1,
+    '-': 1,
+    '*': 2,
+    '/': 2
+  }
+
+  operations = {
+    '+': function (a, b) { return b + a },
+    '-': function (a, b) { return b - a },
+    '*': function (a, b) { return b * a },
+    '/': function (a, b) { return b / a },
+  }
+
+  let inputArr = expr.split('').filter(el => el != ' ');
+
+  // console.log(inputArr);
+
+  for (let i = 1; i < inputArr.length; i++) {
+    if ((+inputArr[i] >= 0) && (+inputArr[i-1]>=0)) {
+      inputArr[i-1] = inputArr[i-1] + inputArr[i];
+      inputArr.splice(i , 1);
+      i -= 1;
+    }
+  }
+
+  // console.log(inputArr);
+
+  let exprToRpn = [];
+  let stack = [];
+
+  for (let i = 0; i < inputArr.length; i++) {
+    if (+inputArr[i] >= 0) exprToRpn.push(+inputArr[i]);
+    else if (inputArr[i] === '(') stack.push(inputArr[i]);
+    else if (inputArr[i] === ')') {
+      while (stack[stack.length - 1] != '(') {
+        if (stack.length < 1) throw new Error ('ExpressionError: Brackets must be paired');
+        exprToRpn.push(stack.pop());
+      }
+      stack.pop();
+    } else if ((inputArr[i] === '+') || (inputArr[i] === '-') || (inputArr[i] === '*') || (inputArr[i] === '/')) {
+        if (operPriortity[[stack[stack.length - 1]]] >= operPriortity[inputArr[i]]) exprToRpn.push(stack.pop());
+        stack.push(inputArr[i]);
+      }
+  }
+
+  while (stack.length > 0) exprToRpn.push(stack.pop());
+
+  // console.log(exprToRpn);
+
+  for (let i = 0; i < exprToRpn.length; i++) {
+    if (typeof(exprToRpn[i]) == 'number') stack.push(exprToRpn[i]);
+    else {
+      stack.push(operations[exprToRpn[i]](stack.pop(), stack.pop()));
+    }
+    // console.log(stack);
+  }
+  return stack[0];
 }
 
 module.exports = {
